d80fe7cc1db0f39f40f28fba15f9c02c2ec232bd
bschaap40 <143346586+bschaap40@users.noreply.github.com>
2024-03-01 19:39:06 -0600

    Change allowed host

--- TechExercise/settings.py
+++ TechExercise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p7uqj644le^1_v*)onvf5t-)sh^vy$g1@p$_r7rre5$1l8-(kf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djangoapp']
 
 
 # Application definition
