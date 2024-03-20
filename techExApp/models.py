da19b5052830103a5482b91e0d036a9591133642
bschaap40 <143346586+bschaap40@users.noreply.github.com>
2024-03-20 14:20:52 -0500

    Create Player model

--- techExApp/models.py
+++ techExApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    position = models.CharField(max_length=100)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.name
